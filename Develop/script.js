788aefd8d020de70e4ddb3dc0bf09f240b94749f
Liem Nguyen <tim@Tims-MBP.attlocal.net>
2020-04-30 21:32:24 -0700

    60% complete

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,15 @@ let b = Date.now()
 let ampm
 // console.log("abc " + a)
 // console.log(b)
+let userData = []
 
 
 
 for(let i = 0 ; i < 24; i++)
 {
     (i > 12)? ampm = 'PM' : ampm = "AM"
-    
+
+
 let newElem = document.createElement('div')
 newElem.innerHTML = `  
 <div class = "row">
@@ -23,20 +25,69 @@ newElem.innerHTML = `
 document.getElementById('container').append(newElem)
 }
 
+
+// Query items 
 let timeList = document.querySelector('#container')
 let descList = timeList.querySelectorAll('#description')
 let hourList = timeList.querySelectorAll('.hour')
+
+
 
 document.addEventListener('click', (event) =>
 {
     if(event.target.id === "saveBtn")
     {
-    console.log(hourList[event.target.dataset.btnindex].textContent)
+    let tempData = new Object()
+    tempData.time = hourList[event.target.dataset.btnindex].textContent
+    tempData.task = descList[event.target.dataset.btnindex].value
 
-    console.log(hourList[event.target.dataset.btnindex])
+    let tempIndex = userData.findIndex(x => x.time === `${tempData.time}`)
+    let tempLength = descList[event.target.dataset.btnindex].value.length 
+
+        
+    if( tempLength > 0 && tempIndex === -1)
+    {
+        console.log("is pushed")
+       userData.push(tempData)
+       console.log(userData.findIndex(x => x.time === `${tempData.time}`))
+       console.log(userData)
+       setData()
     }
-    
-  
-   
+    else
+    {
+    userData[tempIndex].task = descList[event.target.dataset.btnindex].value
+    console.log(userData)
+    setData()
+    }
+}
 
 })
+
+
+
+/** Store task and time into client-side storage */
+let setData = () =>
+{
+    localStorage.setItem("data", JSON.stringify(userData))
+}
+
+/** Check and get high score from client-side storage */
+const getData = () =>
+{
+        return JSON.parse(localStorage.getItem('data'))
+}
+userData = getData()
+
+// for(let i = 0;i<userData.length;i++)
+// {
+//     console.log(userData)
+   
+//     if(hourList[i].textContent === userData[i].time)
+//     {
+//         console.log("match")
+//         console.log
+//     }
+//     else{
+//         console.log("not match")
+//     }
+// }
